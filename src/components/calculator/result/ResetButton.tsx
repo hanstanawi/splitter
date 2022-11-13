aad1b87aa2a734b4cde873e36562e37b562b29e9
Hans Tanawi <hans.tanawi@outlook.com>
2022-11-13 23:24:40 +0800

    feat: add disabled state to reset button

--- src/components/calculator/result/ResetButton.tsx
+++ src/components/calculator/result/ResetButton.tsx
@@ -1,22 +1,57 @@
+import {
+  useBillValue,
+  useContextDispatch,
+  usePeopleQuantity,
+} from 'hooks/calculator.hooks';
 import styled from 'styled-components';
 
-const StyledResetButton = styled.button`
+type StyledResetButtonProps = {
+  disabled: boolean;
+};
+
+const StyledResetButton = styled.button<StyledResetButtonProps>`
   background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.darkPrimary};
+  opacity: ${({ disabled }) => {
+    return disabled ? '30%' : '100%';
+  }};
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  cursor: pointer;
+  cursor: ${({ disabled }) => {
+    return disabled ? 'auto' : 'pointer';
+  }};
   border: none;
   font-weight: 600;
-  font-size: 1.25rem;
+  font-size: 1rem;
   padding: 0.5rem 0;
   border-radius: 0.25rem;
+  font-family: sans-serif;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.darkPrimary};
+  }
 `;
 
 function ResetButton() {
-  return <StyledResetButton>Reset</StyledResetButton>;
+  const billValue = useBillValue();
+  const peopleQuantity = usePeopleQuantity();
+  const dispatch = useContextDispatch();
+  const isDefaultState = !billValue && !peopleQuantity;
+
+  const resetHandler = () => {
+    dispatch({ type: 'SET_BILL_VALUE', payload: 0 });
+    dispatch({ type: 'SET_TIP_PERCENTAGE', payload: 0 });
+    dispatch({ type: 'SET_PEOPLE_QUANTITY', payload: 0 });
+  };
+
+  return (
+    <StyledResetButton disabled={isDefaultState} onClick={resetHandler}>
+      RESET
+    </StyledResetButton>
+  );
 }
 
 export default ResetButton;
